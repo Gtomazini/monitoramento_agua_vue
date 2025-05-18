2e012ca8ee5b26f107913938f0c5b85ab4e2b0b7
Gabriel Tomazini <gtomazini@hotmail.com>
2025-05-18 18:33:05 -0300

    restrict area

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -14,35 +14,35 @@
         </section>
       </router-link>
       
-      <router-link to="/consumo">
+      <router-link to="/consumo" class="side-disabled" tabindex="-1" @click.prevent>
         <section class="item_side_menu">
           <img src="@/assets/images/side_menu/chart-bold.png" alt="Consumo">
           CONSUMO
         </section>
       </router-link>
       
-      <router-link to="/alertas">
+      <router-link to="/alertas" class="side-disabled" tabindex="-1" @click.prevent>
         <section class="item_side_menu">
           <img src="@/assets/images/side_menu/notification-fill.png" alt="Alertas">
           ALERTAS
         </section>
       </router-link>
       
-      <router-link to="/preferencias">
+      <router-link to="/preferencias" class="side-disabled" tabindex="-1" @click.prevent>
         <section class="item_side_menu">
           <img src="@/assets/images/side_menu/gear.png" alt="Preferências">
           PREFERÊNCIA
         </section>
       </router-link>
       
-      <router-link to="/dispositivos">
+      <router-link to="/dispositivos" class="side-disabled" tabindex="-1" @click.prevent>
         <section class="item_side_menu">
           <img src="@/assets/images/side_menu/device.png" alt="Dispositivos">
           DISPOSITIVOS
         </section>
       </router-link>
       
-      <router-link to="/perfil">
+      <router-link to="/perfil" class="side-disabled" tabindex="-1" @click.prevent>
         <section class="item_side_menu">
           <img src="@/assets/images/side_menu/perfil.png" alt="Perfil">
           PERFIL
@@ -60,4 +60,10 @@
   <style scoped>
   /* Você pode manter os estilos da sidebar aqui se quiser */
   /* Ou pode deixar vazio se estiver importando seu CSS global no App.vue */
+  .side-disabled {
+    pointer-events: none;
+    filter: grayscale(1) brightness(1.2);
+    opacity: 0.6;
+    cursor: not-allowed !important;
+  }
   </style>
